5261f3526da548091b2d525e487e0cf5dd80be3f
qizhiqiang <icyaway@gmail.com>
2018-10-09 16:54:02 +0800

    Use custom formatting tool instead of previous lua-fmt

--- server/providers/format-provider.js
+++ server/providers/format-provider.js
@@ -2,10 +2,15 @@
 const fmt = require('lua-fmt');
 const langserver = require('vscode-languageserver');
 const awaiter = require('./lib/awaiter');
+const child_process = require('child_process');
+const iconv_lite_1 = require('iconv-lite');
+const path_1 = require('path');
+const default_luafmt_executor = path_1.resolve(__dirname, '../../3rd/luafmt/luafmt.exe');
 
 class FormatProvider {
     constructor(coder) {
         this.coder = coder;
+        this.encoding = 'gbk';
     }
 
     formatOnTyping(params) {
@@ -38,7 +43,28 @@ class FormatProvider {
                 text = text.substring(document.offsetAt(range.start), document.offsetAt(range.end));
             }
 
-            let formattedText = fmt.formatText(text, this._formatOptions(opt));
+            let inputBuffer = iconv_lite_1.encode(text, this.encoding);
+            let args = [];
+
+            let options = this._formatOptions(opt);
+            if (options.useTabs) {
+                args.push('--use-tabs');
+            }
+            args.push('--indent-count');
+            args.push(options.indentCount);
+
+            if (options.formatComments) {
+                args.push('--format-comments');
+            }
+
+            let outputBuffer = child_process.execFileSync(default_luafmt_executor, args, {
+                input: inputBuffer,
+                windowsHide: true,
+            })
+
+            let formattedText = iconv_lite_1.decode(outputBuffer, this.encoding);
+
+            // let formattedText = fmt.formatText(text, this._formatOptions(opt));
 
             return [langserver.TextEdit.replace(range, formattedText)];
         });
@@ -48,7 +74,9 @@ class FormatProvider {
         return {
             lineWidth: userOptions.lineWidth || 120,
             indentCount: userOptions.indentCount || 4,
-            quotemark: userOptions.quotemark || 'single'
+            quotemark: userOptions.quotemark || 'single',
+            useTabs: userOptions.useTabs,
+            formatComments: userOptions.formatComments,
         };
     }
 };
